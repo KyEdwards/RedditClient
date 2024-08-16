2d440a53a56b81c079354b5e927ad970b3aa19ba
KyEdwards <123298031+KyEdwards@users.noreply.github.com>
2024-08-16 14:34:34 +0100

    Finnished making a simple counter to work out how to use redux.

--- src/index.js
+++ src/index.js
@@ -3,54 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware  } from 'redux';
+import allReducers from './reducers'; 
+import { Provider } from 'react-redux';
+
+ 
 
 
 
-//Store
-//The store holds all the data for the application. -- A globolised state
+const store = createStore(allReducers,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-//Action
-//It describes what you want to do. IE I go to a store and i'm hungry (but not the actual buying of the food)
 
-//action increment
-const increment = () =>{
-  return {
-    type: 'INCREMENT'
-  }
-}
-const decrement = () =>{
-  return {
-    type: 'DECREMENT'
-  }
-}
-//Reducer 
-// Describes how your actions will interact with the store
-const counter = (state = 0, action) => {
-    switch(action.type){
-      case "INCREMENT":
-        return state + 1;
-      case "DECREMENT":
-        return state - 1
-    }
-}
 
-let store = createStore(counter);
 
-//Display it in the console
-store.subscribe( () => console.log(store.getState()));
 
-//Dispatch
-//How to exicute the action.
-store.dispatch(increment());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
-
+//.render is no longer used you have to create a const and do <React.strictMode> tags
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
